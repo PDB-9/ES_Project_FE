b0abc678d8be49548b9a150af713237093ddd8d1
Ali Yusuf <ali.yusuf@ui.ac.id>
2020-12-22 20:16:13 +0700

    revert env

--- src/pages/MusicPage/utils.js
+++ src/pages/MusicPage/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setIsLoading } from "../../actions/globalActions";
 
-const base_url = process.env.REACT_APP_API_KEY +'/spotify/';
+const base_url = "http://api-spotipy.herokuapp.com/spotify/";
 
 const filterMapping = {
   title: "name",
@@ -55,7 +55,7 @@ export const fetchMusic = async (
 
 export const fetchTopSearch = async () => {
   try {
-    const response = await axios.get(process.env.REACT_APP_API_KEY +'/log/get/');
+    const response = await axios.get("http://api-spotipy.herokuapp.com/log/get/");
     const data = response.data.search;
 
     return data;
